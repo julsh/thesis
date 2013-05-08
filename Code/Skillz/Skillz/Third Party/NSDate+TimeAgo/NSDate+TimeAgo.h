68ee04e7dbe812de6b43e4b31abf87c01a5ea13d
julsh <julia_hro@gmx.de>
2013-05-08 04:39:24 +0200

    removed main repo

--- Code/Skillz/Skillz/Third Party/NSDate+TimeAgo/NSDate+TimeAgo.h
+++
@@ -1,6 +0,0 @@
-@interface NSDate (TimeAgo)
-- (NSString *) timeAgo;
-- (NSString *) timeAgoWithLimit:(NSTimeInterval)limit;
-- (NSString *) timeAgoWithLimit:(NSTimeInterval)limit dateFormat:(NSDateFormatterStyle)dFormatter andTimeFormat:(NSDateFormatterStyle)tFormatter;
-@end
-
